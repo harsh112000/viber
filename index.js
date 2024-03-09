ae65f145325e6220b60ff9250ddbe085b62fc7fd
harsh1111 <harsh.srivastava@zoopindia.com>
2024-03-09 16:53:43 +0530

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
+const path = require("path");
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -9,14 +10,17 @@ const io = require("socket.io")(server, {
 	}
 });
 
+
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 app.get('/', (req, res) => {
-	res.send('Running');
+	res.send('path.join(__dirname, "client/build", index.html")');
 });
 
+app.static(path.join(__dirname,      'client/build'));
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
 
@@ -27,6 +31,8 @@ io.on("connection", (socket) => {
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
 	});
+
+	
 
 	socket.on("answerCall", (data) => {
 		io.to(data.to).emit("callAccepted", data.signal)
